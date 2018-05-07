11a96f30d85fdd92d4504844e023e9150ead9794
Ahmed <ahmed.opeyemi@gmail.com>
2018-05-08 00:32:07 +0100

    Implement call backs for get all links

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ from bs4 import BeautifulSoup
 from remote import getPage
 
 HTML_PARSER = 'html.parser'
+ALL_ZOOS = []
+ALL_GORILLAS = []
 
-main_html_page = getPage()
-main_page_dom = BeautifulSoup(main_html_page, HTML_PARSER)
 
-for zoo_link in main_page_dom.find_all('a'):
-    zoo_link_href = zoo_link.get('href')
-    print(zoo_link_href)
-    zoo_page_dom = BeautifulSoup(getPage(zoo_link_href), HTML_PARSER)
-    
+def get_all_links(page_route, each_link_callback=None):
+    page_dom = BeautifulSoup(getPage(page_route), HTML_PARSER)
+    for link in page_dom.find_all('a'):
+        href = link.get('href')
+        if each_link_callback:
+            each_link_callback(href)
 
+
+def add_zoo(zoo_route):
+    print("Zoo: ", zoo_route)
+    ALL_ZOOS.append(zoo_route)
+
+
+def get_gorilla_info(gorilla_route):
+    print("Gorilla: ", gorilla_route)
+
+
+get_all_links("USAmenu.htm", add_zoo)
+if len(ALL_ZOOS) > 0:
+    for zoo_route in ALL_ZOOS:
+        get_all_links(zoo_route, get_gorilla_info)

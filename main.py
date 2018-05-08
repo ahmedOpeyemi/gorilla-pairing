584bb6277cadb12d6d69747920ede03a1a37f256
Ahmed <ahmed.opeyemi@gmail.com>
2018-05-08 02:02:15 +0100

    Offspring and siblings retrieval

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Third party imports
 from bs4 import BeautifulSoup
+import re
 
 # Local imports
 from remote import getPage
 
 HTML_PARSER = 'html.parser'
 ALL_ZOOS = []
-ALL_GORILLAS = []
+
+SEX_LABEL = "Sex"
+DATE_OF_DEATH_LABEL = "Date of death"
+FATHER_LABEL = "Sire"
+MOTHER_LABEL = "Dam"
 
 
 def get_all_links(page_route, each_link_callback=None):
@@ -18,15 +23,40 @@ def get_all_links(page_route, each_link_callback=None):
 
 
 def add_zoo(zoo_route):
-    print("Zoo: ", zoo_route)
+    # print("Zoo: ", zoo_route)
     ALL_ZOOS.append(zoo_route)
 
 
 def get_gorilla_info(gorilla_route):
-    print("Gorilla: ", gorilla_route)
+    gorilla = {
+        'link': gorilla_route,
+        'alive': True
+    }
+    gorilla_page_dom = BeautifulSoup(getPage(gorilla_route), HTML_PARSER)
+    sex_tag = gorilla_page_dom.find_all(string=re.compile(SEX_LABEL))[0]
+    if sex_tag:
+        gorilla['sex'] = "Female" if "Female" in sex_tag.string else "Male"
+    date_of_death_tag = gorilla_page_dom.find_all(
+        string=re.compile(DATE_OF_DEATH_LABEL)
+    )
+    if len(date_of_death_tag) > 0:
+        gorilla['alive'] = False
+    siblings_header_tag = gorilla_page_dom.find(text=re.compile("Siblings")).parent.parent
+    siblings_tag = siblings_header_tag.find_next_sibling("p")
+    if siblings_tag:
+        for sibling in siblings_tag.find_next_siblings():
+            print('sibling >>> ', sibling)
+            '''
+            for each sibliin, check if it is a, if yes, get the name, if not,
+            check if it is offspring text, if yes, then offspring listing has started, if yes,
+            all others shold be marked as offsprings
+            '''
+        # print('siblings tag >>>', siblings_tag.children, siblings_tag.descendants, siblings_tag.contents)
 
+    print('Gorilla >>>', gorilla)
 
 get_all_links("USAmenu.htm", add_zoo)
 if len(ALL_ZOOS) > 0:
     for zoo_route in ALL_ZOOS:
         get_all_links(zoo_route, get_gorilla_info)
+        break

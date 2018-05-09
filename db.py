031db3366674a9900b605ecd7b02652db52a3658
Ahmed <ahmed.opeyemi@gmail.com>
2018-05-09 21:49:33 +0100

    Implement insert_or_update_gorilla function

--- db.py
+++ db.py
@@ -16,52 +16,102 @@ def get_or_create_connection(db_path, destroy_if_exists=False):
 
 
 def create_tables(connection):
-    CREATE_GORILLA_TABLE_SCRIPT = '''
-        CREATE TABLE IF NOT EXISTS gorilla(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            gid TEXT UNIQUE,
-            name TEXT,
-            link TEXT UNIQUE,
-            alive INTEGER NULL,
-            sex TEXT NULL,
-            sire TEXT NULL,
-            dam TEXT NULL,
-            FOREIGN KEY(sire) REFERENCES gorilla(gid)
-            FOREIGN KEY(dam) REFERENCES gorilla(gid)
-        )
-    '''
+    try:
+        CREATE_GORILLA_TABLE_SCRIPT = '''
+            CREATE TABLE IF NOT EXISTS gorilla(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                gid TEXT UNIQUE,
+                name TEXT,
+                link TEXT UNIQUE,
+                alive INTEGER NULL,
+                sex TEXT NULL,
+                sire TEXT NULL,
+                dam TEXT NULL,
+                FOREIGN KEY(sire) REFERENCES gorilla(gid)
+                FOREIGN KEY(dam) REFERENCES gorilla(gid)
+            )
+        '''
 
-    CREATE_SIBLINGS_TABLE_SCRIPT = '''
-        CREATE TABLE IF NOT EXISTS siblings(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            gid TEXT,
-            sibling_id TEXT,
-            FOREIGN KEY(gid) REFERENCES gorilla(gid)
-            FOREIGN KEY(sibling_id) REFERENCES gorilla(gid)
-        )
-    '''
+        CREATE_SIBLINGS_TABLE_SCRIPT = '''
+            CREATE TABLE IF NOT EXISTS siblings(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                gid TEXT,
+                sibling_id TEXT,
+                FOREIGN KEY(gid) REFERENCES gorilla(gid)
+                FOREIGN KEY(sibling_id) REFERENCES gorilla(gid)
+            )
+        '''
 
-    CREATE_OFFSPRINGS_TABLE_SCRIPT = '''
-        CREATE TABLE IF NOT EXISTS offsprings(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            gid TEXT,
-            offspring_id TEXT,
-            FOREIGN KEY(gid) REFERENCES gorilla(gid)
-            FOREIGN KEY(offspring_id) REFERENCES gorilla(gid)
-        )
-    '''
+        CREATE_OFFSPRINGS_TABLE_SCRIPT = '''
+            CREATE TABLE IF NOT EXISTS offsprings(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                gid TEXT,
+                offspring_id TEXT,
+                FOREIGN KEY(gid) REFERENCES gorilla(gid)
+                FOREIGN KEY(offspring_id) REFERENCES gorilla(gid)
+            )
+        '''
 
-    cur = connection.cursor()
-    for table_script in [
-        CREATE_GORILLA_TABLE_SCRIPT,
-        CREATE_SIBLINGS_TABLE_SCRIPT,
-        CREATE_OFFSPRINGS_TABLE_SCRIPT
-    ]:
-        cur.execute(table_script)
+        cursor = connection.cursor()
+        for table_script in [
+            CREATE_GORILLA_TABLE_SCRIPT,
+            CREATE_SIBLINGS_TABLE_SCRIPT,
+            CREATE_OFFSPRINGS_TABLE_SCRIPT
+        ]:
+            cursor.execute(table_script)
+            connection.commit()
+    except Exception as ex:
+        print("Error: ", ex)
 
 
-def insert_or_update_gorilla(gorilla):
-    pass
+# TODO: A lot of field repetition in this project. Use constants to replace.
+
+
+def insert_or_update_gorilla(gorilla, connection):
+    try:
+        record_exist = False
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT COUNT(*) from gorilla where gid={}".format(gorilla['gid']))
+        (number_of_rows,) = cursor.fetchone()
+        record_exist = number_of_rows > 0
+        script = ''
+        if record_exist:
+            script = '''
+                UPDATE gorilla SET (
+                    alive=coalesce(alive, {}),
+                    sex=coalesce(sex, {}),
+                    sire=coalesce(sire, {}),
+                    dam=coalesce(dam, {})
+                )
+                WHERE gid={}
+            '''.format(
+                1 if gorilla['alive'] is True else 0,
+                gorilla['sex'],
+                gorilla['sire'],
+                gorilla['dam'],
+                gorilla['gid']
+            )
+        else:
+            script = '''
+                INSERT INTO gorilla(gid, name, link, alive, sex, sire, dam)
+                VALUES({}, {}, {}, {}, {}, {}, {})
+            '''.format(
+                gorilla['gid'], gorilla['name'],
+                gorilla['link'], gorilla['alive'],
+                gorilla['sex'], gorilla['sire'],
+                gorilla['dam']
+            )
+        cursor.execute(script)
+        connection.commit()
+        print("Gorilla {} inserted/updated successfully.".format(
+            gorilla['gid']))
+    except Exception as ex:
+        print("Error: ", ex)
+    finally:
+        pass
+        # connection.close()
+
 
 
 def insert_sibling(gorilla, sibling):

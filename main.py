ddc714e7d3d24c48bf133e09a0341f954d13922c
Ahmed <ahmed.opeyemi@gmail.com>
2018-05-09 20:31:39 +0100

    Append offsprings to the list

--- main.py
+++ main.py
@@ -37,15 +37,18 @@ def make_identifier(href, name):
 
 def get_gorilla_info(gorilla_route):
     gorilla = {
+        'identifier': None,
         'link': gorilla_route,
         'alive': True,
         'sex': None,
         'siblings': [],
-        'offspring': [],
+        'offsprings': [],
         'sire': None,  # Father
         'dam': None,  # Mother
     }
     gorilla_page_dom = BeautifulSoup(getPage(gorilla_route), HTML_PARSER)
+    name = (gorilla_page_dom.find_all("font", attrs={"size": 5})[0]).string
+    gorilla['identifier'] = make_identifier(gorilla_route, name)
     sex_tag = gorilla_page_dom.find_all(string=re.compile(SEX_LABEL))[0]
     if sex_tag:
         gorilla['sex'] = "Female" if "Female" in sex_tag.string else "Male"
@@ -54,20 +57,21 @@ def get_gorilla_info(gorilla_route):
     )
     if len(date_of_death_tag) > 0:
         gorilla['alive'] = False
-    siblings_header_tag = gorilla_page_dom.find(text=re.compile("Siblings")).parent.parent
+    siblings_header_tag = gorilla_page_dom.find(
+        text=re.compile("Siblings")).parent.parent
     siblings_tag = siblings_header_tag.find_next_sibling("p")
     has_completed_siblings_tag = False
     if siblings_tag:
         for sibling in siblings_tag.find_next_siblings():
             if sibling.name == 'a' and has_completed_siblings_tag == False:
-                gorilla['offspring'].append(make_identifier(sibling.get(HREF_TAG), sibling.string))
+                gorilla['siblings'].append(
+                    make_identifier(sibling.get(HREF_TAG), sibling.string))
             elif sibling.name == 'font':
-                print('font value >>> ', sibling.string)
-            '''
-            for each sibling, check if it is a, if yes, get the name, if not,
-            check if it is offspring text, if yes, then offspring listing has started, if yes,
-            all others shold be marked as offsprings
-            '''
+                if len(sibling.find_all(text=re.compile("Offspring"))) > 0:
+                    has_completed_siblings_tag = True
+            elif sibling.name == 'a' and has_completed_siblings_tag == True:
+                gorilla['offsprings'].append(
+                    make_identifier(sibling.get(HREF_TAG), sibling.string))
         # print('siblings tag >>>', siblings_tag.children, siblings_tag.descendants, siblings_tag.contents)
 
     print('Gorilla >>>', gorilla)

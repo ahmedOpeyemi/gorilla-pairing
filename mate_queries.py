467bf23ef5598ca061cf871de3d4fec733081423
Ahmed <ahmed.opeyemi@gmail.com>
2018-05-10 22:58:45 +0100

    Add grand_parents query

--- mate_queries.py
+++ mate_queries.py
@@ -10,11 +10,20 @@ Queries for all the relation types.
 
 
 def build_query(gid, sex):
+    parent_label = 'sire' if sex == 'M' else 'dam'
     return {
         'grand_children': '''
-                SELECT offspring_id FROM offsprings WHERE gid IN (
-                    SELECT offspring_id FROM offsprings WHERE gid='{0}'
-                    AND sex='{1}'
-                ) AND sex='{1}'
-            '''.format(gid, sex)
+                SELECT offspring_id FROM offsprings a
+                LEFT JOIN gorilla b ON a.offspring_id = b.gid
+                WHERE a.gid IN (
+                    SELECT offspring_id FROM offsprings c
+                    LEFT JOIN gorilla d ON c.offspring_id = d.gid
+                    WHERE c.gid='{0}' AND d.sex='{1}'
+                ) AND b.sex='{1}'
+            '''.format(gid, sex),
+        'grand_parents': '''
+                SELECT {0} FROM gorilla a WHERE a.gid=(
+                    SELECT {0} FROM gorilla b WHERE b.gid='{1}'
+                )
+        '''.format(parent_label, gid)
     }
